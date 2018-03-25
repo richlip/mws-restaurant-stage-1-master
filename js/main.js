828e9c9c2204fe23bc63d4a5e27f5d08c32303b2
richard <richlip@googlemail.com>
2018-03-25 20:44:01 +0200

    added changes
    role and small refinements

--- js/main.js
+++ js/main.js
@@ -151,7 +151,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name + ' Main Image';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h6');
   name.innerHTML = restaurant.name;
   li.append(name);
 
