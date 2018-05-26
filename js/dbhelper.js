b48e9c7b1c75fa0ae0e42bb8b036eec4ccc3dafa
richard <richlip@googlemail.com>
2018-05-26 20:50:44 +0200

    performance +
    added webp

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -192,7 +192,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    return (`/img/${restaurant.photograph}.webp`);
   }
 
   /**
